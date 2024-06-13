326209c07f01bc3b9349c990c0f15f1f054b5ba3
mike-ward <mike@mike-ward.net>
2024-06-13 13:35:11 -0500

    implement windows version of read_link

--- src/ls/ls_windows.c.v
+++ src/ls/ls_windows.c.v
@@ -1,22 +1,20 @@
 #include <Windows.h>
-#include <stdio.h>
+#include <unistd.h>
 
+fn readlink(path &char, link &chr, len usize) int
 fn C.LookupAccountSid(sys_name &char, sid &char, sid_size voidptr, domain_name &char, domain_name_size voidptr, use voidptr) bool
 
 fn name_from_id(sid usize) string {
-	name_size := 256
-	name := '\0'.repeat(buf_size)
-	domain_hamesize := 256
-	domain := '\0'.repeat(domain_size)
+	mut name_size := 256
+	name := '\0'.repeat(buf_size + 1)
+	mut domain_hamesize := 256
+	domain := '\0'.repeat(domain_size + 1)
 	sidType := 0
 
-	if C.LookupAccountSid(void_ptr(0), sid.str, name.str, voidptr(name_size), domain.str,
-		voidptr(domain_size), voidptr(sidType))
-	{
-		return name.substr(0, name_size).cstring_to_vstring()
-	} else {
-		return sid.str()
-	}
+	success := C.LookupAccountSid(void_ptr(0), sid.str, name.str, voidptr(name_size),
+		domain.str, voidptr(domain_size), voidptr(sidType))
+
+	return if success { name.substr(0, name_size) } else { sid.str() }
 }
 
 fn get_owner_name(uid usize) string {
@@ -28,5 +26,8 @@ fn get_group_name(uid usize) string {
 }
 
 fn read_link(file string) string {
-	return '?'
+	size := 2048
+	link = '\0'.repeat(size + 1)
+	len := C.readlink(file.str, link.str, size)
+	return if len > 0 { link.substr(0, len) } else { '' }
 }
