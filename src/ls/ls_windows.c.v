c2d1bf5ad4f242e1ffa74ad2feffde0a906f3230
mike-ward <mike@mike-ward.net>
2024-06-14 17:50:02 -0500

    avoid windows calls for now

--- src/ls/ls_windows.c.v
+++ src/ls/ls_windows.c.v
@@ -1,33 +1,11 @@
-#include <Windows.h>
-#include <unistd.h>
-
-fn readlink(path &char, link &chr, len usize) int
-fn C.LookupAccountSid(sys_name &char, sid &char, sid_size voidptr, domain_name &char, domain_name_size voidptr, use voidptr) bool
-
-fn name_from_id(sid usize) string {
-	mut name_size := 256
-	name := '\0'.repeat(buf_size + 1)
-	mut domain_hamesize := 256
-	domain := '\0'.repeat(domain_size + 1)
-	sidType := 0
-
-	success := C.LookupAccountSid(void_ptr(0), sid.str, name.str, voidptr(name_size),
-		domain.str, voidptr(domain_size), voidptr(sidType))
-
-	return if success { name.substr(0, name_size) } else { sid.str() }
-}
-
 fn get_owner_name(uid usize) string {
-	return name_from_id(uid)
+	return uid.str()
 }
 
 fn get_group_name(uid usize) string {
-	return name_from_id(uid)
+	uid.str()
 }
 
 fn read_link(file string) string {
-	size := 2048
-	link = '\0'.repeat(size + 1)
-	len := C.readlink(file.str, link.str, size)
-	return if len > 0 { link.substr(0, len) } else { '' }
+	return '?'
 }
