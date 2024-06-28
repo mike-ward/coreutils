8bfcf02aa2684cda652c947d01647d094a5d74cf
mike-ward <mike@mike-ward.net>
2024-06-28 09:57:04 -0500

    WIP

--- src/sort/sort.v
+++ src/sort/sort.v
@@ -1,3 +1,26 @@
+import os
+
 fn main() {
-	get_options()
+	options := get_options()
+	results := sort(options)
+	for result in results {
+		print(result)
+	}
+}
+
+fn sort(options Options) []string {
+	mut results := []string{}
+	for file in options.files {
+		results << do_sort(file, options)
+	}
+	return results
+}
+
+fn do_sort(file string, options Options) []string {
+	mut lines := os.read_lines(file) or { exit_error(err.msg()) }
+	match true {
+		options.ignore_case { lines.sort_ignore_case() }
+		else { lines.sort() }
+	}
+	return lines
 }
